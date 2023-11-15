755b4f5df5b84bde91fdf0c8a8ddd03c5cf0c9be
Alec Aivazis <alec@aivazis.com>
2023-11-15 09:58:08 -0800

    exercise 19

---
+++ 19.routing-and-data-fetching/src/app.jsx
@@ -0,0 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+
+// the URL to get the list of repos: https://api.github.com/legacy/repos/search/react?sort=stars
+// the URL to get the details of facebook/react: https://api.github.com/repos/facebook/react
+
+export function RepoList() {}
