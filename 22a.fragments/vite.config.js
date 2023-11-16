8b1951ec4dffa7e19b27342083fc4f45de35f62d
Alec Aivazis <alec@aivazis.com>
2023-11-16 10:06:48 -0800

    remove cruft

--- 22a.fragments/vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from "@vitejs/plugin-react";
-import relay from "../22.relay/node_modules/vite-plugin-relay/dist/plugin";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [relay, react()],
-});
